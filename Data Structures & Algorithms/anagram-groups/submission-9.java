413909a10ac9c4faccf84aaf4b21e7cd261ff0a6
Meow <97556969+ttyuli2002@users.noreply.github.com>
2026-06-14 17:39:04 -0700

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> hMap = new HashMap<>();
+        for (String str: strs) {
+            char[] charStr = str.toCharArray();
+            Arrays.sort(charStr);
+            String key = new String(charStr);
+
+            hMap.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(hMap.values());
+    }
+}
